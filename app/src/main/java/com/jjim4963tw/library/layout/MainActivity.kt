520e4bb68ef7b22124f9291cc132e26d727db622
Jim <jim.huang@asuscloud.com>
2020-09-14 15:30:34 +0800

    add : add jetpack library and example

--- app/src/main/java/com/jjim4963tw/library/layout/MainActivity.kt
+++ app/src/main/java/com/jjim4963tw/library/layout/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.jjim4963tw.library.R
+import com.jjim4963tw.library.layout.jetpack.JetPackMainActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +16,12 @@ class MainActivity : AppCompatActivity() {
 
     fun openStorageAndMediaPage(view: View) {
         Intent(this, StorageAndMediaActivity::class.java).run {
+            startActivity(this)
+        }
+    }
+
+    fun openJerPackPage(view: View) {
+        Intent(this, JetPackMainActivity::class.java).run {
             startActivity(this)
         }
     }
