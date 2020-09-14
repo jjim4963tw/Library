5ae615137f24092f11ed6ccbc63266bd0fcb6d2c
Jim <jim.huang@asuscloud.com>
2020-09-14 13:36:40 +0800

    add : add main page

--- app/src/main/java/com/jjim4963tw/library/layout/MainActivity.kt
+++ app/src/main/java/com/jjim4963tw/library/layout/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.jjim4963tw.library.layout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.jjim4963tw.library.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+    fun openStorageAndMediaPage(view: View) {
+        Intent(this, StorageAndMediaActivity::class.java).run {
+            startActivity(this)
+        }
     }
 }
