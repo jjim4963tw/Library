48081e90a1b9c51eafd62ba93bcb88901e3b54d7
Jim <jim.huang@ecareme.com>
2021-03-24 13:40:31 +0800

    update : update library version

--- app/src/main/java/com/jjim4963tw/library/layout/jetpack/DataStoreActivity.kt
+++ app/src/main/java/com/jjim4963tw/library/layout/jetpack/DataStoreActivity.kt
@@ -1,5 +1,6 @@
 package com.jjim4963tw.library.layout.jetpack
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.datastore.core.DataStore
@@ -18,14 +19,11 @@ import java.io.IOException
 
 class DataStoreActivity : AppCompatActivity() {
 
-    private lateinit var dataStore: DataStore<Preferences>
+    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "setting")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_data_store)
-
-        // init DataStore
-        dataStore = createDataStore(name = "setting")
 
         lifecycleScope.launch {
             dataStore.data.first()
